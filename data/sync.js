42a12bdd523898d8b9a97a5a142bd205c150fe72
Irene <irene.iaccio@gmail.com>
2019-03-30 22:46:16 +0100

    optimizations

--- data/sync.js
+++ data/sync.js
@@ -12,41 +12,51 @@ function createIndex(index) {
     if(err) {
       if (err.status === 400) {
         console.log(chalk.blue(index + ' already exists. Skip..'))
-        populateIndex(index, page)
+        fetchItems(index, page)
       } else {
         console.log(chalk.red(err))
       }
     } else {
       console.log(chalk.green('[' + status +'] Created index: ' + resp.index))
-      populateIndex(index, page)
+      fetchItems(index, page)
     }
   })
 }
 
-function populateIndex(index, page) {
-  fetch('https://api.discogs.com/users/' + config.user.name + '/inventory?page='+ page.toString() +'&per_page=100&token=' + config.user.token).then(response => {
-    return response.json();
-  }).then(resp => {
-    if (resp.listings) {
-      console.log(chalk.blue('Import ' + index + '['+ page +']..'))
-      resp.listings.forEach((record) => {
-        client.index({
-          index: index,
-          id: record.id,
-          type: 'records',
-          body: record
-        },(err,item,status)=> {
-          console.log(chalk.green(status) + ' id: ' + item._id)
+function fetchItems(index, page) {
+    fetch('https://api.discogs.com/users/' + config.user.name + '/inventory?status=for sale&page='+ page.toString() +'&per_page=5&token=' + config.user.token).then(response => {
+      return response.json();
+    }).then(resp => {
+      if (resp.listings) {
+        console.log(chalk.blue('Import ' + index + '['+ page +']..'))
+        resp.listings.forEach((record) => {
+          populateIndex(index, record)
         })
-      })
-      if (resp.pagination.pages > page) {
-        populateIndex(index, page + 1)
-      } else {
-        console.log(resp.pagination.items + ' items imported')
+        if (resp.pagination.pages > page) {
+          setTimeout(() => {
+            fetchItems(index, page + 1)
+          }, 0)
+        } else {
+          console.log(resp.pagination.items + ' items imported')
+        }
       }
+    }).catch(error =>{
+      console.log(chalk.red(error))
+    })
+}
+
+function populateIndex (index, record) {
+  client.index({
+    index: index,
+    id: record.id,
+    type: 'records',
+    body: record
+  },(err,item,status)=> {
+    if(status === 200) {
+      console.log(chalk.green(status) + ' id: ' + record.id)
+    } else {
+      console.log(chalk.red(status) + ' id: ' + record.id)
     }
   })
 }
-
-
 
